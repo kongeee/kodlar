2a1863b3894aa8d78bcdf70771fa19a8fdae5894
kongeee <furkanekici1997@gmail.com>
2021-07-10 13:19:14 +0300

    c# first commit

---
+++ C#/1_KampIntro/1_KampIntro/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+
+namespace _1_KampIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //DEĞİŞKENLER
+            string isim = "Furkan Ekici";
+            int yas = 23;
+            long buyukSayi = 32154687874;
+            double faiz = 2.15;
+            bool sistemeGirisYapildiMi = false;
+            
+            Console.WriteLine(isim);
+
+            //IF-ELSE YAPISI
+            if(sistemeGirisYapildiMi == true){
+
+                Console.WriteLine("Sisteme giriş yapıldı");
+            }
+            else{
+
+                Console.WriteLine("Giriş yap!");
+            }
+
+
+            //DÖNGÜLER
+
+            //array tanımlama
+            string[] kurslar = new string[] { "C#", "Java", "NodeJS" };
+
+            Console.WriteLine("\nKurslar :");
+            for(int i = 0; i < kurslar.Length; i++) {//FOR
+                Console.WriteLine(kurslar[i]+"(FOR)");
+            }
+
+            foreach(string kurs in kurslar) {//FOR-EACH
+                Console.WriteLine(kurs+"(FOR-EACH)");
+            }
+            
+
+
+
+
+
+
+        }
+    }
+}
---
+++ C#/1_KampIntro/2_ClassIntro/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _2_ClassIntro {
+    class Program {
+        static void Main(string[] args) {
+
+            //Obje oluşturma
+            Kurs kurs1 = new Kurs();
+            kurs1.KursAdi = "Java";
+            kurs1.Egitmen = "Engin Demiroğ";
+            kurs1.IzlenmeOrani = 84;
+
+            Kurs kurs2 = new Kurs();
+            kurs2.KursAdi = "C#";
+            kurs2.Egitmen = "Furkan Ekici";
+            kurs2.IzlenmeOrani = 47;
+
+            Kurs kurs3 = new Kurs();
+            kurs3.KursAdi = "JavaScript";
+            kurs3.Egitmen = "Eren Ekici";
+            kurs3.IzlenmeOrani = 66;
+
+            //class array
+            Kurs[] kurslar = new Kurs[] { kurs1, kurs2, kurs3 };
+
+            foreach(Kurs kurs in kurslar) {//Kurs yerine var da yazabilirdik. -> foreach(var kurs in kurslar)
+                Console.WriteLine(kurs.KursAdi + " : " + kurs.Egitmen + "\n" + kurs.IzlenmeOrani + "% izlendi\n");
+            }
+
+
+        }
+    }
+
+
+    //CLASS OLUSTURMA
+    class Kurs {
+        public string KursAdi { get; set; }
+        public string Egitmen { get; set; }
+
+        public int IzlenmeOrani { get; set; }
+    }
+}
